c320223e41fe707279ac88aebe22c9454790f5df
Cheng-G-T <1211239560@qq.com>
2026-07-03 16:37:55 +0900

    fix: remove next.config.js env to let Vercel inject runtime env vars

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    DEEPSEEK_API_KEY: process.env.DEEPSEEK_API_KEY || '',
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
